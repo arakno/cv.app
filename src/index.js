56a497b01deb7ef42197460d31ac7360dc027e02
arakno <paulobasto@gmail.com>
2020-07-05 20:58:17 +0100

    ini

---
+++ src/index.js
@@ -0,0 +1,134 @@
+import print from 'print-js';
+import Sortable from 'sortablejs'
+import { Experience, Education } from './templates'
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const PATHS = {
+    css: './css/'
+  }
+
+  const cv = document.getElementById('cv')
+
+  const profile = document.getElementById('profile')
+  const intro = document.getElementById('intro')
+
+	const experiences = cv.querySelectorAll('#cv-Experience article')
+  const educations = cv.querySelectorAll('#cv-Education article')
+
+  const certs = document.getElementById('cv-Certifications')
+  const papers = document.getElementById('papers')
+  const readings = document.getElementById('readings')
+  const volunteer = document.getElementById('volunteer')
+  const tools = document.getElementById('tools')
+
+  const sections = [profile, intro, certs, papers, readings, volunteer, tools]
+
+  //HELPER FUNCTIONS FOR EASY DOM MANIPULATION
+	/** @{function} - helper global $elector alias */
+	window.$ = function(selector) {
+		let selectorType = 'querySelectorAll';
+
+		if (selector.indexOf('#') === 0) {
+			selectorType = 'getElementById';
+			selector = selector.substr(1, selector.length);
+    }
+  }
+    
+  /** @{function} - helper method to attach events */
+  Node.prototype.on = window.on = Node.prototype.addEventListener;
+  NodeList.prototype.on = NodeList.prototype.addEventListener = function(name, fn) {
+    Array.prototype.slice.call(this).forEach(function(el) {
+      el.addEventListener(name, fn, false);
+    });
+  }
+  
+    
+  // Add visible/hidden checkboxes to each section
+  for (let section of sections) {
+    let input = document.createElement('input')
+    input.type = 'checkbox'
+    section.parentNode.insertBefore(input, section)
+  }
+
+  // Add visible/hidden checkboxes to each experience
+	for (let experience of experiences) {
+    let input = document.createElement('input')
+    input.type = 'checkbox'
+    // experience.insertBefore(input, experience)
+    // experience.prepend(input)
+    experience.parentNode.insertBefore(input, experience)
+  }
+  
+  for (let education of educations) {
+    let input = document.createElement('input')
+    input.type = 'checkbox'
+    education.parentNode.insertBefore(input, education)
+  }
+  
+
+  const printBtn = document.getElementById('print-btn')
+  printBtn.on('click', () => {
+    const primaryCSS = document.createElement('link')
+    primaryCSS.href = `${PATHS.css}stacks.css`
+    primaryCSS.rel = 'stylesheet'
+    primaryCSS.type = 'text/css'
+    const secondaryCSS = document.createElement('link')
+    secondaryCSS.href = `${PATHS.css}primary.css`
+    secondaryCSS.rel = 'stylesheet'
+    secondaryCSS.type = 'text/css'
+    const tertiaryCSS = document.createElement('link')
+    tertiaryCSS.href = `${PATHS.css}pdf.css`
+    tertiaryCSS.media = 'print'
+    tertiaryCSS.rel = 'stylesheet'
+    tertiaryCSS.type = 'text/css'
+
+    const doc = document.getElementById('print_frame').contentWindow.document;
+    doc.head.appendChild(primaryCSS);
+    doc.head.appendChild(secondaryCSS);
+    doc.head.appendChild(tertiaryCSS);
+    doc.body.innerHTML = document.getElementById('cv').innerHTML
+
+    setTimeout(() => {
+      window.frames['print_frame'].focus();
+      window.frames['print_frame'].print();
+    }, 500)
+  })
+
+  cv.on('click', (evt) => {
+    const isButton = evt.target.nodeName === 'BUTTON'
+    if (!isButton) {
+      return
+    }
+
+    if (evt.target.classList.contains('add-btn')) {
+      let section = evt.target.closest('section')
+      section.insertAdjacentHTML('beforeend', Experience)
+      // section.nextElementSibling.insertAdjacentHTML('beforeend', Experience)
+      // console.dir(evt.target.id)
+    }
+  })
+
+  // Drag n Drop functionality
+  const experienceList = cv.querySelector('.experience')
+  Sortable.create(experienceList, { 
+    animation: 150 
+  })
+
+  const educationList = cv.querySelector('.education')
+  Sortable.create(educationList, { 
+    animation: 150 
+  })
+
+
+  // TODO: Add localStorage with localforage?
+  // localforage.setItem('key', 'value').then(fu
+  //   nction () {
+  //   return localforage.getItem('key');
+  // }).then(function (value) {
+  //   // we got our value
+  // }).catch(function (err) {
+  //   // we got an error
+  // });
+
+});
---
+++ src/templates.js
@@ -0,0 +1,80 @@
+// import holder from 'holderjs'
+
+const uid = new Date().getTime()
+
+export const Experience = `<input type="checkbox">
+<article draggable="true" class="grid gs16" id="Experience-${uid}">
+
+                                <div class="grid--cell">
+                                    <div class="g-center cv-logo-blank
+                                        js-logo-blank dno"><svg aria-hidden="true" class="svg-icon
+                                            iconIndustry" width="18" height="18" viewBox="0 0 18 18"><path d="M10
+                                                16v-4H8v4H2V4c0-1.1.9-2 2-2h6a2
+                                                2 0 0 1 2 2v2h2a2 2 0 0 1 2
+                                                2v8h-6zM4 4v2h2V4H4zm0
+                                                4v2h2V8H4zm4-4v2h2V4H8zm0
+                                                4v2h2V8H8zm-4 4v2h2v-2H4zm8
+                                                0v2h2v-2h-2zm0-4v2h2V8h-2z"></path></svg></div>
+                                    <div class="g-center cv-logo">
+                                        <img src="https://via.placeholder.com/48" role="img" class="cv-image
+                                            js-list-img anonymizable" title="placeholder" onerror="handleListImageError(this)" >
+                                    </div>
+                                </div>
+
+                                <div class="grid--cell fl1 fd-column">
+                                    <h3 contenteditable="true" class="grid--cell mb2 fs-subheading
+                                        fc-black-800">
+                                        Role
+                                    </h3>
+
+                                    <h4 class="grid--cell mb8 fs-body2
+																				anonymizable">
+																				<a contenteditable="true" href="" rel="nofollow" title="..." target="_blank">Company</a>
+																		</h4>
+
+                                    <div class="grid--cell mb8">
+                                        <span class="fs-caption fc-black-700">From → To</span>
+                                        <span class="fs-caption fc-black-500">(Duration)</span>
+                                    </div>
+
+                                    <div class="mtn8 mb16">
+                                        <div class="timeline-item-tags g-col
+                                            g-row fl-none ff-row-wrap mt16">
+                                            <span class="post-tag">Tech tag</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="fs-body1 fc-black-700">
+                                        <p contenteditable="true">Description...<div><br></div></p>
+                                    </div>
+                                </div>
+														</article>`
+														
+	export const Education = `<input type="checkbox">
+	<article class="grid gs16 first" id="Education-${uid}">
+
+	<div class="grid--cell">
+			<div class="g-center cv-logo">
+				<img src="https://via.placeholder.com/48" role="img" class="cv-image" title="placeholder" onerror="handleListImageError(this)">
+			</div>
+	</div>
+
+	<div class="grid--cell fl1 fd-column">
+			<h3 contenteditable="true" class="grid--cell mb2 fs-subheading
+					fc-black-800">
+					Course
+			</h3>
+
+			<h4 contenteditable="true" class="grid--cell mb8 fs-body2
+					fc-black-500">Institution</h4>
+
+			<div class="grid--cell mb8">
+					<span class="fs-caption fc-black-700">from
+							→ to</span>
+			</div>
+
+			<div class="fs-body1 fc-black-700">
+					<p contenteditable="true">Accomplishments</p>
+			</div>
+	</div>
+</article>`
